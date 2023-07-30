8d959411a3c195314e110fbf604f6830fa722a03
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2023-07-30 14:31:19 +0200

    Create pmmpStatsAsync.php

---
+++ core/pmmpStatsAsync.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace YOURNAME\PLUGINNAME\pmmpStats;
+
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+
+class pmmpStatsAsync extends AsyncTask {
+    protected string $url;
+
+    function __construct(string $url) {
+        $this->url = $url;
+    }
+
+    public function onRun() : void {
+        // Send the http request
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        $data = json_decode($data);
+        if ($data->status !== 200) {
+            echo "ERROR WHILE PUTTING DATA ON pmmpStats, server says: '{$data->message}'\n";
+        }
+    }
+}
